32b5189597a89de9c97041721d396243eb96291c
Alamgir Ahamed Saif <36258593+alamgir72435@users.noreply.github.com>
2021-03-27 12:16:31 +0600

    intigreted pusher and mongoose

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,45 @@
+import React, { useEffect, useState } from "react";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
 import "./App.css";
+import Pusher from "pusher-js";
+import axios from "./axios";
+// import Scroll from "./scroll";
 function App() {
+  const [message, setMessage] = useState([]);
+
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessage(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    var pusher = new Pusher("2199f1671725b34159f8", {
+      cluster: "ap2",
+    });
+
+    var channel = pusher.subscribe("messages");
+    channel.bind("inserted", function (newMessage) {
+      // console.log(JSON.stringify(newMessage));
+      setMessage((prevMessages) => [...prevMessages, newMessage]);
+    });
+
+    // cleanup
+    return () => {
+      channel.unsubscribe();
+      channel.unbind_all();
+    };
+  }, []);
+
+  console.log(message);
+
   return (
     <div className="app">
       <div className="app__body">
+        {/* <Scroll /> */}
         <Sidebar />
-        <Chat />
+        <Chat messages={message} />
       </div>
     </div>
   );
--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,12 +1,37 @@
 import { Avatar, IconButton } from "@material-ui/core";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import "./Chat.css";
 import { InsertEmoticon } from "@material-ui/icons";
 import MicIcon from "@material-ui/icons/Mic";
-const Chat = () => {
+import axios from "./axios";
+import { Element, animateScroll as scroll } from "react-scroll";
+const Chat = ({ messages }) => {
+  const [message, setMessage] = useState("");
+  // const chat__body = useRef();
+  const chat__body__scroll = () => {
+    scroll.scrollToBottom();
+  };
+
+  useEffect(() => {
+    chat__body__scroll();
+  }, []);
+
+  const sendMessage = async (e) => {
+    window.scrollTo(0, 1000);
+    e.preventDefault();
+    await axios.post("/messages/new", {
+      message: message,
+      name: "Alamgir ahamed",
+      timestamp: Date.now().toString(),
+      received: true,
+    });
+    setMessage("");
+    chat__body__scroll();
+  };
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -29,28 +54,35 @@ const Chat = () => {
         </div>
       </div>
       <div className="chat__body">
-        <p className="chat__message">
-          <span className="chat__name">Alamgir</span>
-          This is a Message
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
-        <p className="chat__message chat__reciever">
-          <span className="chat__name">Alamgir</span>
-          This is a Message
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
-        <p className="chat__message">
-          <span className="chat__name">Alamgir</span>
-          This is a Message
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
+        <Element>
+          {messages.map((message, index) => (
+            <p
+              key={index + 1}
+              className={`chat__message ${
+                message.received && "chat__reciever"
+              }`}
+            >
+              <span className="chat__name">{message.name}</span>
+              {message.message}
+              <span className="chat__timestamp">{message.timestamp}</span>
+            </p>
+          ))}
+        </Element>
+
+        {/* chat__reciever */}
       </div>
 
       <div className="chat__footer">
         <InsertEmoticon />
         <form>
-          <input placeholder="Type a message" />
-          <button>Send a message</button>
+          <input
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+            placeholder="Type a message"
+          />
+          <button type="submit" onClick={sendMessage}>
+            Send a message
+          </button>
         </form>
         <MicIcon />
       </div>
